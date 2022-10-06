7dc3dda1bb87a1f6ea7753b23e8ba250142b1b96
Janette <janette.castillo91@yahoo.com>
2022-10-06 09:11:33 -0500

    adding files

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+const toggleButton = document.querySelector(".toggle-button")
+const navbarLinks = document.querySelector(".nav-menu")
 const reservationForm = document.querySelector(".reserve")
 const reserveContainer = document.querySelector('.reserve-form')
 const customerForm = document.querySelector(".customer-info")
 
+toggleButton.addEventListener('click', navMenu)
 reservationForm.addEventListener('submit', customerInfo)
 customerForm.addEventListener('submit', confirmReservation)
+
+
+function navMenu() {
+    navbarLinks.classList.toggle('active')
+  }
 
 function customerInfo(event) {
     event.preventDefault();
